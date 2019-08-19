dc2ee83ebb3c5f485c994eb0d03a1767f5b2952a
William Cameron <wp.cameron@gmail.com>
2019-08-19 20:15:50 +0100

    test vue instance instantiates

--- tests/specs/App.spec.js
+++ tests/specs/App.spec.js
@@ -1,3 +1,11 @@
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue';
+
 it('works', () => {
     expect(true).toBe(true);
+});
+
+it('is a Vue instance', () => {
+    const wrapper = shallowMount(App);
+    expect(wrapper.isVueInstance()).toBeTruthy()
 });
