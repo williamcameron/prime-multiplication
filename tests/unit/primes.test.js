fd74ef8fae5e883064a38fec977294c763366a8b
William Cameron <wp.cameron@gmail.com>
2019-08-19 22:03:36 +0100

    failing tests

--- tests/unit/primes.test.js
+++ tests/unit/primes.test.js
@@ -20,3 +20,11 @@ test('request 3 primes returns first 3', () => {
     expect(getFirstPrimes(3).length).toEqual(3);
     expect(getFirstPrimes(3)).toBe([2,3,5]);
 });
+
+
+test('returns first 2 primes', () => {
+    expect(getFirstPrimes(2)).toEqual([2,3]);
+});
+test('returns first 10 primes', () => {
+    expect(getFirstPrimes(10)).toEqual([2,3,5,7,11,13,17,19,23,29]);
+});
