adedd317bae5c44ad50ee309539aea3c5b0311e9
William Cameron <wp.cameron@gmail.com>
2019-08-19 22:17:31 +0100

    tests and a working algorithm

--- src/Primes.js
+++ src/Primes.js
@@ -1,5 +1,34 @@
 function getFirstPrimes(numberToFetch) {
-    return new Array(numberToFetch).fill(2);
+    if (typeof numberToFetch !== 'number' || parseInt(numberToFetch) <= 0) {
+        return [];
+    }
+
+    let primes = [];
+    let checkingNumber = 2;
+
+    while (primes.length < numberToFetch) {
+        if (isPrime(checkingNumber)) {
+            primes.push(checkingNumber);
+        }
+        checkingNumber++;
+    }
+    return primes;
+
 }
 
-export { getFirstPrimes };
+function isPrime(number) {
+    if (typeof number !== 'number' || parseInt(number) <= 0) {
+        return false;
+    }
+    if (number < 2) {
+        return false;
+    }
+    for (var i = 2; i < number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+export { getFirstPrimes, isPrime };
--- tests/unit/primes.test.js
+++ tests/unit/primes.test.js
@@ -1,30 +1,64 @@
-import { getFirstPrimes } from '@/Primes';
+import { getFirstPrimes, isPrime } from '@/Primes';
 
 test('sanity', () => {
     expect(true).toBe(true);
 });
 
-test('request primes returns array', () => {
-    const anyInput = 0;
-    expect(getFirstPrimes(anyInput)).toStrictEqual([]);
-});
-test('request 1 primes returns 2', () => {
-    expect(getFirstPrimes(1)).toStrictEqual([2]);
+describe('Testing getFirstPrimes function', () => {
+
+    test('request primes returns array', () => {
+        const anyInput = 0;
+        expect(getFirstPrimes(anyInput)).toStrictEqual([]);
+    });
+    test('request 1 primes returns 2', () => {
+        expect(getFirstPrimes(1)).toEqual([2]);
+    });
+
+    test('request 5 returns array length 5', () => {
+        expect(getFirstPrimes(5).length).toEqual(5);
+    })
+
+    test('request 3 primes returns first 3', () => {
+        expect(getFirstPrimes(3).length).toEqual(3);
+        expect(getFirstPrimes(3)).toEqual([2,3,5]);
+    });
+
+
+    test('returns first 2 primes', () => {
+        expect(getFirstPrimes(2)).toEqual([2,3]);
+    });
+    test('returns first 10 primes', () => {
+        expect(getFirstPrimes(10)).toEqual([2,3,5,7,11,13,17,19,23,29]);
+    });
+
+    test('returns nothing for string input', () => {
+        expect(getFirstPrimes('string input')).toEqual([]);
+    });
+
+    test('returns blank array for negative input', () => {
+        expect(getFirstPrimes(-1)).toEqual([]);
+    });
+
 });
 
-test('request 5 returns array length 5', () => {
-    expect(getFirstPrimes(5).length).toEqual(5);
-})
-
-test('request 3 primes returns first 3', () => {
-    expect(getFirstPrimes(3).length).toEqual(3);
-    expect(getFirstPrimes(3)).toBe([2,3,5]);
-});
-
-
-test('returns first 2 primes', () => {
-    expect(getFirstPrimes(2)).toEqual([2,3]);
-});
-test('returns first 10 primes', () => {
-    expect(getFirstPrimes(10)).toEqual([2,3,5,7,11,13,17,19,23,29]);
+describe('Testing isPrime function', () => {
+    test('returns true for 2', () => {
+        expect(isPrime(2)).toBeTruthy();
+    });
+    test('returns false for 4', () => {
+        expect(isPrime(4)).toBe(false);
+    });
+    test('returns false for 1', () => {
+        expect(isPrime(1)).toBeFalsy();
+    });
+    test('returns true for 29', () => {
+        expect(isPrime(29)).toBeTruthy();
+    });
+    test('returns false for string', () => {
+        expect(isPrime('some string')).toBe(false);
+    });
+    test('returns false for negative', () => {
+        expect(isPrime(-10)).toBe(false);
+    });
+    
 });
