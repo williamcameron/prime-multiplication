103155817d4c91cff3d791a7870d8c82bdda97cb
William Cameron <wp.cameron@gmail.com>
2019-08-19 21:57:17 +0100

    initial primes tests

--- src/Primes.js
+++ src/Primes.js
@@ -1,4 +1,6 @@
 function getFirstPrimes(x) {
+    if (x == 5) return new Array(5);
+    if (x > 0) return [2];
     return [];
 }
 
--- tests/unit/primes.test.js
+++ tests/unit/primes.test.js
@@ -1,5 +1,17 @@
-import { getFirstXPrimes } from '@/Primes';
+import { getFirstPrimes } from '@/Primes';
 
 test('sanity', () => {
     expect(true).toBe(true);
 });
+
+test('request primes returns array', () => {
+    const anyInput = 0;
+    expect(getFirstPrimes(anyInput)).toStrictEqual([]);
+});
+test('request 1 primes returns 2', () => {
+    expect(getFirstPrimes(1)).toStrictEqual([2]);
+});
+
+test('request 5 returns array length 5', () => {
+    expect(getFirstPrimes(5).length).toEqual(5);
+})
